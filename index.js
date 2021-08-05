e8860f9097ca471313979729ca96c45a769d7c33
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2021-08-05 18:51:15 +0500

    craete server

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get("/", (req, res) =>{
+    const filePath = path.join(__dirname, 'public', 'index.html')
+    res.sendFile(filePath, err =>{
+        if(err) throw err;
+    })
+
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>{
+    console.log(`Server started on port ${PORT}...`);
+})
