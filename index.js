55a1cbdfde340982be5e761e928db301c80b1a1a
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2021-08-05 22:00:06 +0500

    fetch video  infos from youtube

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
+const http = require('http')
+const ytdl = require('ytdl-core')
 const path = require('path')
 const app = express()
 
-app.use(express.json())
-app.use(express.static('public'))
+// app.use(express.json())
+// app.use(express.static('public'))
 
 app.get("/", (req, res) =>{
     const filePath = path.join(__dirname, 'public', 'index.html')
     res.sendFile(filePath, err =>{
         if(err) throw err;
     })
+})
+
+app.get('/videoInfo', async (req, res) =>{
+    const videoURL = req.query.videoURL;
+    const info = await ytdl.getInfo(videoURL);
+    res.status(200).json(info);
 
 })
+
 
 const PORT = process.env.PORT || 5000;
 
