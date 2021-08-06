6f0666fa9bff82fa2e23ec5cbac95a3ca34c2d1d
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2021-08-06 20:27:56 +0500

    commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/download', (req, res) =>{
 })
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () =>{
     console.log(`Server started on port ${PORT}...`);
